adab4b2d9d9546e3ce4b15af9fec516fff9a4a7b
nabeel-midnay <nabeel@midnay.com>
2026-01-13 17:48:32 +0530

    remove class wgeb div disappear

--- assets/js/frontend-cart.js
+++ assets/js/frontend-cart.js
@@ -234,7 +234,7 @@
 
             var newTimerId = setTimeout(function () {
                 $el.fadeOut(300, function () {
-                    $(this).removeClass("woo-limit-error").removeData("error-timer");
+                    $(this).removeClass("woo-limit-error woo-limit-count-error").removeData("error-timer");
                     if (typeof onHide === "function") onHide();
                 });
             }, duration || 5000);
@@ -370,7 +370,7 @@
         // Hide all limit-related error messages
         function hideAllLimitErrors() {
             $(SEL.limitMessage).hide().removeClass("woo-limit-error");
-            $(SEL.quantityMessage).hide().removeClass("woo-limit-error");
+            $(SEL.quantityMessage).hide().removeClass("woo-limit-error woo-limit-count-error");
             $(".woo-limit.woo-limit-error").removeClass("woo-limit-error");
             $(".woo-limit.woo-limit-empty-error").removeClass("woo-limit-empty-error");
             $(".woo-number-range.woo-limit-error").removeClass("woo-limit-error");
@@ -647,6 +647,8 @@
                         }
 
                         var $rowErr = $wrapper.find(".woo-limit-quantity-message").first();
+                        // Add custom class for limit count errors
+                        $rowErr.addClass("woo-limit-count-error");
                         showTimedError($rowErr, msg, 5000);
                         $qty.val(oldQty);
                         finalizeQty(oldQty);
